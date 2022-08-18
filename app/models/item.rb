8bc8c0cb75a1fa29d96ca67a8f7cb3a4d9bca3a6
KN-tiger <108563821+KN-tiger@users.noreply.github.com>
2022-08-18 11:54:15 +0000

    [add]商品詳細・一覧ページ追加・itemモデルに税計算メソッド追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,18 +1,25 @@
 class Item < ApplicationRecord
-    
+
     belongs_to :genre
     has_many :order_items
     has_many :carts
+
+    has_one_attached :item_image
     
-    has_one_attached :image
     
-    
-    def get_image
-      unless image.attached?
-        file_path = Rails.root.join('app/assets/images/no_image.jpg')
-        image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-      end
-      image.variant(resize_to_limit: [width, height]).processed
+    def price_with_tax
+      tax = 1 + 0.10
+      ( self.price_without_tax * tax).floor
     end
     
+    def get_item_image#(width, height)
+      unless item_image.attached?
+        file_path = Rails.root.join('app/assets/images/no_image.jpg')
+        item_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      end
+      item_image
+      # item_image.variant(resize_to_limit: [width, height]).processed
+      # gemのインストールが必要なため相談
+    end
+
 end
