b49fea8fd63938d1cdaa52caf9d53cbddc0b5341
KN-tiger <108563821+KN-tiger@users.noreply.github.com>
2022-08-18 12:32:32 +0000

    [fix]Itemモデルget_item_imageメソッドの削除,viewの編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,21 +5,10 @@ class Item < ApplicationRecord
     has_many :carts
 
     has_one_attached :item_image
-    
-    
+
+
     def price_with_tax
       tax = 1 + 0.10
       ( self.price_without_tax * tax).floor
     end
-    
-    def get_item_image#(width, height)
-      unless item_image.attached?
-        file_path = Rails.root.join('app/assets/images/no_image.jpg')
-        item_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-      end
-      item_image
-      # item_image.variant(resize_to_limit: [width, height]).processed
-      # gemのインストールが必要なため相談
-    end
-
 end
