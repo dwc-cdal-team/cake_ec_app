b308e932924166f9e2675e30d1294f01ab6a90c8
EC2 Default User <ec2-user@ip-172-31-30-140.ec2.internal>
2022-08-19 05:27:46 +0000

    マージ作業

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -12,5 +12,10 @@ class Public::CustomersController < ApplicationController
   end
 
   def deactivate
+    @customer = Customer.find(params[:id])
+    @cistomer.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行しました"
+    redirect_to root_path
   end
 end
