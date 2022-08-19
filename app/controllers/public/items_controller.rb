720a0620c26fb93fb32a17dc6c3655e4016b7f7c
KN-tiger <108563821+KN-tiger@users.noreply.github.com>
2022-08-19 10:52:33 +0000

    [add]商品一覧ページのview調整

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,10 +1,12 @@
 class Public::ItemsController < ApplicationController
   def index
     @items = Item.page(params[:page])
+    @all_items = Item.all
+
   end
 
   def show
     @item = Item.find(params[:id])
-    @cart = Cart.new
+    
   end
 end
