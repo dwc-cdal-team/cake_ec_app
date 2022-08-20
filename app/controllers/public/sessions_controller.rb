3ce479be77aabb8fac7024e67c377ebc9fc59827
EC2 Default User <ec2-user@ip-172-31-30-140.ec2.internal>
2022-08-20 09:50:25 +0000

    マージ作業

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -6,10 +6,10 @@ class Public::SessionsController < Devise::SessionsController
   def customer_state
     @customer = Customer.find_by(email: params[:customer][:email])
     return if !@customer
-    if @customer.valid_password?(params[:customer][:password]) == false
-      redirect_to new_customer_registration_path
-    elsif @customer.is_deleted == true
-      redirect_to new_customer_registration_path
+    if @customer.valid_password?(params[:customer][:password])
+      if @customer.is_deleted
+        redirect_to new_customer_registration_path
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
 
-  # URL /customers/sign_in ...
-  devise_for :customers,skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
-
-  # URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-    sessions: "admin/sessions"
-  }
-
   scope module: :public do
     root to: "homes#top"
     get '/about', to: 'homes#about', as: 'about'
@@ -27,6 +16,12 @@ Rails.application.routes.draw do
     resources :destinations, except: [:show,:new]
   end
 
+    # URL /customers/sign_in ...
+    devise_for :customers,skip: [:passwords], controllers: {
+      registrations: "public/registrations",
+      sessions: 'public/sessions'
+    }
+
   namespace :admin do
     root to: "homes#top"
     resources :items, except: [:destroy]
@@ -35,6 +30,12 @@ Rails.application.routes.draw do
     resources :orders, only: [:show,:update]
     resources :order_items, only: [:update]
   end
+
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
