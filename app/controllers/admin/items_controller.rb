44f3cf2c874f45efa332e9ed24271b51649b870d
tominaga1653 <108568710+tominaga1653@users.noreply.github.com>
2022-08-20 06:43:10 +0000

    [add]商品編集機能

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,19 +1,48 @@
 class Admin::ItemsController < ApplicationController
+
   def index
+    @items = Item.page(params[:page]).per(10)
   end
 
   def new
+    @item_new = Item.new
+    @genres = Genre.all
   end
 
   def create
+    @item_new = Item.new(item_params)
+    if @item_new.save
+      redirect_to admin_item_path(@item_new), notice: "商品が登録されました。"
+    else
+      @genres = Genre.all
+      render :new
+    end
   end
 
   def show
+    @item = Item.find(params[:id])
+    @genre = Genre.find(@item.genre_id)
   end
 
   def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item), notice: "変更内容が保存されました。"
+    else
+      @genres = Genre.all
+      render :edit
+    end
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :genre_id, :name, :introduction, :price_without_tax, :is_stopped)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,16 @@
 class Item < ApplicationRecord
-    
+
     belongs_to :genre
     has_many :order_items
     has_many :carts
-    
+
     has_one_attached :image
-    
+
+    validates :genre_id, presence: true
+    validates :name, presence: true
+    validates :introduction, presence: true
+    validates :price_without_tax, presence: true
+    validates :is_stopped, inclusion: {in: [true, false]}
+    validates :image, presence: true
+
 end
