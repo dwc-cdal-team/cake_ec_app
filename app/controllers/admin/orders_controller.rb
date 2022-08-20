656417b94427a0fdc72e29b65b0b5140ac229541
EC2 Default User <108569722+ShingoIshikawa@users.noreply.github.com>
2022-08-20 09:35:10 +0000

    [add]注文詳細画面のアイテム一覧表示機能の実装

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,11 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @total_price = 0
+
+    @order.order_items.each do |order_item|
+      @total_price += (order_item.price_with_tax * order_item.amount)
+    end
   end
 
   def update
