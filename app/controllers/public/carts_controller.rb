ed5e5cf995048d9dad5d4b29d437544afa9f86c5
KN-tiger <108563821+KN-tiger@users.noreply.github.com>
2022-08-20 11:25:57 +0000

    [add]カートコントローラcreate

--- app/controllers/public/carts_controller.rb
+++ app/controllers/public/carts_controller.rb
@@ -1,16 +1,40 @@
 class Public::CartsController < ApplicationController
+  
   def index
+    @carts = current_customer.carts
   end
 
   def update
+    
   end
 
   def destroy
+    
   end
 
   def destroy_all
+    
   end
 
   def create
+    
+    @cart = Cart.new(cart_params)
+    @cart.customer_id = current_customer.id
+    if @cart.save
+      redirect_to carts_path
+    else
+      @item = Item.find_by(id:@cart.item_id)
+      redirect_to item_path(@item.id)
+      flash[:notice] = "個数を選択して下さい"
+    end
+    
   end
+
+  
+  private
+  
+  def cart_params
+    params.require(:cart).permit(:item_id, :amount)
+  end
+  
 end
