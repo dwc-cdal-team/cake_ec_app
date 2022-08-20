c82b4062943050a102d1a0d702659116e1e12579
KN-tiger <108563821+KN-tiger@users.noreply.github.com>
2022-08-20 14:32:02 +0000

    [add]cart コントローラーviewの編集

--- app/controllers/public/carts_controller.rb
+++ app/controllers/public/carts_controller.rb
@@ -1,40 +1,60 @@
 class Public::CartsController < ApplicationController
-  
+
+  before_action :ensure_customer
+  #before_action :setup_cart, only: [:update, :destroy, :destroy_all, :create]
+
   def index
-    @carts = current_customer.carts
+    @carts = current_customer.carts.all
+
   end
 
   def update
-    
+    if @cart.update(amount: params[:amount].to_i)
+      flash[:notice] = 'カート内の商品情報が更新されました'
+    else
+      flash[:notice] = 'カート内の商品情報の更新に失敗しました'
+    end
+    redirect_to carts_path
   end
 
   def destroy
-    
+    if @cart.destroy
+      flash[:notice] = 'カート内の商品が削除されました'
+    else
+      flash[:notice] = '削除に失敗しました'
+    end
+    redirect_to carts_path
   end
 
   def destroy_all
-    
+
   end
 
   def create
-    
+
     @cart = Cart.new(cart_params)
-    @cart.customer_id = current_customer.id
     if @cart.save
       redirect_to carts_path
     else
-      @item = Item.find_by(id:@cart.item_id)
-      redirect_to item_path(@item.id)
-      flash[:notice] = "個数を選択して下さい"
+      redirect_to item_path(@cart.item_id)
     end
-    
+
   end
 
-  
+
   private
-  
+
   def cart_params
-    params.require(:cart).permit(:item_id, :amount)
+    params.require(:cart).permit(:customer_id, :item_id, :amount)
   end
-  
+
+  def ensure_customer
+    redirect_to new_customer_session_path unless customer_signed_in?
+  end
+
+# 　def setup_cart
+#     cart.customer_id = current_customer.id
+#     @cart = Cart.find_by(params[:customer_id])
+# 　end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,14 @@
 class Item < ApplicationRecord
-    
+
     belongs_to :genre
     has_many :order_items
     has_many :carts
-    
+
     has_one_attached :image
-    
+
+    validates :customer_id, presence: true
+    validates :item_id, presence: true
+    validates :amount, presence: true
 
     def price_with_tax
       tax = 1 + 0.10
