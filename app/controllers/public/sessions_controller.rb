e28ea5c85263c7d3166513a2c2dbaac4f2d869be
EC2 Default User <ec2-user@ip-172-31-30-140.ec2.internal>
2022-08-20 03:21:54 +0000

    マージ作業

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -6,10 +6,9 @@ class Public::SessionsController < Devise::SessionsController
   def customer_state
     @customer = Customer.find_by(email: params[:customer][:email])
     return if !@customer
-    if !@customer.valid_password?(params[:customer][:password])
+    if @customer.valid_password?(params[:customer][:password]) == false
       redirect_to new_customer_registration_path
-    end
-    if @customer.is_deleted?
+    elsif @customer.is_deleted == true
       redirect_to new_customer_registration_path
     end
   end
