16f4cb34085e34e73568733e73bfd785c09af068
EC2 Default User <108569722+ShingoIshikawa@users.noreply.github.com>
2022-08-20 02:27:25 +0000

    [add]ジャンル一覧、ジャンル追加機能を実装

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,8 +1,18 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genres = Genre.all
+    @genre_new = Genre.new
   end
 
   def create
+    @genre_new = Genre.new(genre_params)
+    if @genre_new.save
+      redirect_to admin_genres_path
+      flash[:notice] = "ジャンル名を追加しました。"
+    else
+      @genres = Genre.all
+      render :index
+    end
   end
 
   def edit
@@ -10,4 +20,11 @@ class Admin::GenresController < ApplicationController
 
   def update
   end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
 end
