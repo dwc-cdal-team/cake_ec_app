77934a6961f42892c16474d45e0589445f6813a5
EC2 Default User <ec2-user@ip-172-31-30-140.ec2.internal>
2022-08-21 13:59:11 +0000

    [add]配送先の設定

--- app/controllers/public/destinations_controller.rb
+++ app/controllers/public/destinations_controller.rb
@@ -1,27 +1,43 @@
 class Public::DestinationsController < ApplicationController
   def index
     @destination = Destination.new
-    @destinations = Destination.all(current_user)
+    @destinations = Destination.where(customer_id: current_customer.id)
   end
 
   def edit
+    @destination = Destination.find(params[:id])
   end
 
   def create
-    destination = Destination.new
-    destination.save
-    redirect_to request.referer
+    @destination = Destination.new(destination_params)
+    @destination.customer_id = current_customer.id
+    if @destination.save
+      redirect_to destinations_path
+    else
+      @destinations = Destination.where(customer_id: current_customer.id)
+      render :index
+    end
   end
 
   def update
+    @destination = Destination.find(params[:id])
+    if @destination.update(destination_params)
+      redirect_to destinations_path
+      flash[:notice] = "編集した内容を保存しました。"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @destination = Destination.find(params[:id])
+    @destination.destroy
+    redirect_to destinations_path
   end
-  
-  private deatination_params
+
+  private
+
+  def destination_params
     params.require(:destination).permit(:postal_code, :address, :name)
-  def
-  
   end
 end
