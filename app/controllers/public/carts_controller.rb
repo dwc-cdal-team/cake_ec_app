6b1777cf103fcdbc4f6a5027715527650e86bccd
KN-tiger <108563821+KN-tiger@users.noreply.github.com>
2022-08-21 06:20:30 +0000

    [add]カートに追加機能

--- app/controllers/public/carts_controller.rb
+++ app/controllers/public/carts_controller.rb
@@ -1,27 +1,28 @@
 class Public::CartsController < ApplicationController
 
   before_action :ensure_customer
-  #before_action :setup_cart, only: [:update, :destroy, :destroy_all, :create]
+  before_action :setup_cart, only: [:update, :destroy, :destroy_all]
 
   def index
-    @carts = current_customer.cart.all
+    @carts = current_customer.carts.all
+    item = Item.findby(params[:id])
 
   end
 
   def update
     if @cart.update(amount: params[:amount].to_i)
-      flash[:notice] = 'カート内の商品情報が更新されました'
+      flash[:notice] = "カート内の商品情報が更新されました"
     else
-      flash[:notice] = 'カート内の商品情報の更新に失敗しました'
+      flash[:notice] = "カート内の商品情報の更新に失敗しました"
     end
     redirect_to carts_path
   end
 
   def destroy
     if @cart.destroy
-      flash[:notice] = 'カート内の商品が削除されました'
+      flash[:notice] = "カート内の商品が削除されました"
     else
-      flash[:notice] = '削除に失敗しました'
+      flash[:notice] = "削除に失敗しました"
     end
     redirect_to carts_path
   end
@@ -31,14 +32,19 @@ class Public::CartsController < ApplicationController
   end
 
   def create
-
-    @cart = Cart.new(cart_params)
-    if @cart.save
-      redirect_to carts_path
-    else
-      redirect_to item_path(@cart.item_id)
+		@cart = Cart.new(cart_params)
+		@cart.customer_id = current_customer.id
+		@carts = current_customer.carts.all
+		@carts.each do |cart|
+      if cart.item_id == @cart.item_id
+        new_amount = cart.amount + @cart.amount
+        cart.update_attribute(:amount, new_amount)
+        @cart.delete
+      end
     end
-
+    @cart.save
+    flash[:notice] = "カートに商品を追加しました"
+    redirect_to carts_path
   end
 
 
@@ -50,11 +56,12 @@ class Public::CartsController < ApplicationController
 
   def ensure_customer
     redirect_to new_customer_session_path unless customer_signed_in?
+    flash[:notice] = "ご購入にはログインが必要です"
   end
 
-　def setup_cart
+  def setup_cart
     cart.customer_id = current_customer.id
     @cart = Cart.find_by(params[:customer_id])
-　end
+  end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,8 @@ class Item < ApplicationRecord
     validates :customer_id, presence: true
     validates :item_id, presence: true
     validates :amount, presence: true
-
+    
+    
     def price_with_tax
       tax = 1 + 0.10
       ( self.price_without_tax * tax).floor
