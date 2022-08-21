805ce313a2b32a9d2f61ee1af41737c47eab4626
EC2 Default User <ec2-user@ip-172-31-30-140.ec2.internal>
2022-08-21 08:07:57 +0000

    マージ作業

--- app/controllers/public/destinations_controller.rb
+++ app/controllers/public/destinations_controller.rb
@@ -1,16 +1,27 @@
 class Public::DestinationsController < ApplicationController
   def index
+    @destination = Destination.new
+    @destinations = Destination.all(current_user)
   end
 
   def edit
   end
 
   def create
+    destination = Destination.new
+    destination.save
+    redirect_to request.referer
   end
 
   def update
   end
 
   def destroy
+  end
+  
+  private deatination_params
+    params.require(:destination).permit(:postal_code, :address, :name)
+  def
+  
   end
 end
