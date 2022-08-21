0b698d0355c43d7de2258e9f2af41ebd4a17bc82
EC2 Default User <108569722+ShingoIshikawa@users.noreply.github.com>
2022-08-21 06:14:34 +0000

    [add]注文履歴詳細と日本語化対応

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,8 +6,17 @@ class Admin::OrdersController < ApplicationController
     @order.order_items.each do |order_item|
       @total_price += (order_item.price_with_tax * order_item.amount)
     end
+
+    @destination_address = "〒" + @order.postal_code.to_s.insert(3, "-") + " " + @order.address
+#    @order_new = Order.new
+#    @order_item_new = OrderItem.new
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order.update(order_status: params[:order][:order_status])
+    flash[:notice] = "注文ステータスを更新しました。"
+    redirect_to admin_order_path(@order.id)
   end
+
 end
