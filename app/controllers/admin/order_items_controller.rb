d1284abe503a5dc35ebc523b7ecbb1ca5fcfc0cf
EC2 Default User <108569722+ShingoIshikawa@users.noreply.github.com>
2022-08-22 07:03:21 +0000

    [add]注文ステータス、制作ステータスの自動更新機能追加

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -3,6 +3,15 @@ class Admin::OrderItemsController < ApplicationController
   def update
     @order_item = OrderItem.find(params[:id])
     @order_item.update(making_status: params[:order_item][:making_status])
+
+    if @order_item.making_status == "making"
+      @order_item.order.update(order_status: 2)
+    elsif OrderItem.where(order_id: @order_item.order_id).count == OrderItem.where(order_id: @order_item.order_id, making_status: "finish").count
+      #OrderItem.where(making_status: 3).count
+      @order_item.order.update(order_status: 3)
+      #Order.find(id: @order_item.order_id).update(order_status: 3)
+    end
+
     flash[:notice] = "制作ステータスを更新しました。"
     redirect_to admin_order_path(@order_item.order_id)
   end
