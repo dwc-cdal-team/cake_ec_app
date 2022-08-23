21d882b71aefdbfb5fa96f4e3882826c7fce5e19
EC2 Default User <ec2-user@ip-172-31-30-140.ec2.internal>
2022-08-23 22:28:57 +0000

    マージ作業

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -2,6 +2,8 @@ class Public::GenresController < ApplicationController
 
   def show
     @genres = Genre.all
-
+    @genre = Genre.find(params[:id])
+    @all_items = Item.where(genre_id: @genre.id)
+    @items = Item.where(genre_id: @genre.id).page(params[:page]).per(8)
   end
 end
