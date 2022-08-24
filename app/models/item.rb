83e51363f98d0524ae2bf995429cf9dcca55b1c3
tominaga1653 <108568710+tominaga1653@users.noreply.github.com>
2022-08-24 04:58:48 +0000

    [add]バリデーションエラーメッセージの日本語化

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,6 @@ class Item < ApplicationRecord
 
     has_one_attached :image
 
-    validates :genre_id, presence: true
     validates :name, presence: true
     validates :introduction, presence: true
     validates :price_without_tax, presence: true
