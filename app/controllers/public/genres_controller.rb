02473d39e851d72c682abba1ed1f2166a86088b7
EC2 Default User <ec2-user@ip-172-31-30-140.ec2.internal>
2022-08-25 02:48:15 +0000

    [add]件数表示

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -3,7 +3,6 @@ class Public::GenresController < ApplicationController
   def show
     @genres = Genre.all
     @genre = Genre.find(params[:id])
-    @all_items = Item.where(genre_id: @genre.id)
     @items = Item.where(genre_id: @genre.id).page(params[:page]).per(8)
   end
 end
